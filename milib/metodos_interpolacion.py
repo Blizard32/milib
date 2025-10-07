3cb6963b45c5f97241c6df4a47f846c19ee0d0b9
Blizard32 <fouteiral32@gmail.com>
2025-10-07 12:58:13 -0300

    Se agrega interpolación de Newton

--- milib/metodos_interpolacion.py
+++ milib/metodos_interpolacion.py
@@ -50,3 +50,55 @@ def interpol_lagrange(matriz, valor):
         interpolacion += y[i] * peso(x, i, valor)
 
     return interpolacion
+
+
+
+#------------------------------------------------------
+#               Interpolacion de Newton
+#------------------------------------------------------
+
+def a_n(n, x, y):
+    """Calcula el coeficiente a_n,k de la tabla de diferencias divididas
+
+    Args:
+        n (int): Es el indice del coeficiente a calcular
+        k (int): Es el orden actual del coeficiente
+        matriz (vector): Es la matriz que contiene los datos y las diferencias divididas
+    """
+    if n > 1:
+        k = len(x)
+        # print("Comienzo recursividad") # Ayuda para debuggear
+        return (a_n(n-1, x[1:], y[1:]) - a_n(n-1, x[:-1], y[:-1])) / (x[n-1] - x[n-k])
+    else:
+        # print("Se devolvio f(k): ", y) # Ayuda para debuggear
+        return y[0]
+
+def interpol_newton(matriz, valor):
+    """Interpolacion de Newton mediante matriz pasada como argumento
+    Args:
+        matriz (matrix): Es la matriz que contiene los datos y las diferencias divididas
+        valor (float): Es el valor en el que se quiere evaluar la funcion.
+        
+    Returns:
+        polinomio_x (float): Es el valor del polinomio de Newton evaluado en "valor".
+
+    """
+    x = [fila[0] for fila in matriz]  # Se extraen los valores de la columna 1  [x]
+    y = [fila[1] for fila in matriz]  # Se extraen los valores de la columna 2 [f(x)]
+    if len(x) != len(y):
+        raise ValueError("Las listas x e y deben tener la misma longitud.")
+    n = len(x)  # Se evalua la cantidad de puntos que se tienen
+    polinomio_x = 0
+    for i in range(1, n+1):
+        coef = i        # Nos sirve para cuando i = n, cambiar i = n*2, j sigue siendo n
+        if(i == n):
+            i = n*2  # Para cuando tengamos la uultima iteracion, se ejecuta la lista completa
+             
+        # print(x[:-(n-i)], y[:-(n-i)]) # Ayuda para debuggear
+        adicion = a_n(coef, x[:-(n-i)], y[:-(n-i)])
+        for j in range(coef-1):
+            adicion *= (valor - x[j])
+            
+        polinomio_x += adicion
+        
+    return polinomio_x
